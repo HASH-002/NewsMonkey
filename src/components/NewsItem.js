a572e78a58e8d74dc77221bcf64329359534cd69
HASH-002 <koserhasan002@gmail.com>
2021-10-01 10:29:57 +0530

    Revert "Improved UI"
    This reverts commit 6a19f09b4353349ae2ec45ba2bdc3d4b543f975a.

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let { title, description, urlToImage, url, author, date, source } = this.props;
+        let {title, description,urlToImage,url} = this.props;
         return (
-            <div className="card my-3">
-                <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ zIndex: 1, left: '90%' }}>{source}</span>
-                <img src={urlToImage ? urlToImage : 'https://images.news18.com/ibnlive/uploads/2021/09/venus-clouds-life-form-163299369116x9.jpg'} className="card-img-top" alt="..." />
+            <div className="card my-3" style={{width: '18rem'}}>
+                <img src={urlToImage} className="card-img-top" alt="..."/>
                 <div className="card-body">
-                    <h5 className="card-title">{title ? title : "..."}</h5>
-                    <p className="card-text">{description ? description : ""}...</p>
-                    <p className="card-text"><small className="text-muted">By {author ? author : "Unknown"} on {new Date(date).toGMTString()}</small></p>
-                    <a rel="noreferrer" href={url} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                <h5 className="card-title">{title}...</h5>
+                <p className="card-text">{description}...</p>
+                <a rel="noreferrer" href={url} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                 </div>
             </div>
         )
